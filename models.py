bc7c441533db4e076ac2aa21df621d7cac6ce034
Daniela <daniela.hhbello@gmail.com>
2025-07-31 17:12:02 +0100

    info edited

--- models.py
+++ models.py
@@ -1,10 +1,21 @@
 # models.py
+
 from flask_sqlalchemy import SQLAlchemy
 
+# setting up the database connection
 db = SQLAlchemy()
 
+# this is the table where we will store quotes
 class Quote(db.Model):
+    # every quote has a unique id
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.Text, nullable=False)
+
+    # the text of the quote (what it says)
+    text = db.Column(db.String(500), nullable=False)
+
+    # who said the quote
     author = db.Column(db.String(100), nullable=False)
-    tags = db.Column(db.String(200))  # etiquetas separadas por coma
+
+    # extra tags like "life", "motivation" (comma-separated)
+    tags = db.Column(db.String(200))
+
